3bc99360514f63cd6363aed3ceffdb85f6215e2c
Pedro <pedrofurtado@ghr.com>
2025-10-16 18:13:12 -0300

    fix: add instrument para inicializar o sentry

---
+++ src/instrument.ts
@@ -0,0 +1,10 @@
+// src/instrument.ts
+
+import * as Sentry from "@sentry/node";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+});
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,21 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// src/server.ts
+
+import "./instrument.js"; 
 
 import * as Sentry from "@sentry/node";
-import { expressIntegration } from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
-
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
 
 const app = express();
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  integrations: [
-    expressIntegration(),
-    nodeProfilingIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
-});
-
-console.log("Sentry DSN lido pelo app:", process.env.SENTRY_DSN);
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
 
+Sentry.setupExpressErrorHandler(app);
+
 app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000, com Sentry ativado!");
+  console.log("Servidor rodando na porta 3000, com Sentry (nova configuração) ativado!");
 });
