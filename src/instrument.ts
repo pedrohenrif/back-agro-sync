95e5266903a8eb7298f6c5ed13d117038566f51b
Pedro <pedrofurtado@ghr.com>
2025-10-18 15:03:34 -0300

    instrument

--- src/instrument.ts
+++ src/instrument.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("VERIFICAÇÃO DE DEBUG: Porta lida pelo .env:", process.env.PORT);
 console.log("VERIFICAÇÃO DE DEBUG: Sentry DSN lido pelo .env:", process.env.SENTRY_DSN);
 
 Sentry.init({
