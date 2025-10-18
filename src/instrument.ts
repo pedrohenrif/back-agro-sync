9540364b37359230c3b416f35f82b592d367d2e7
Pedro <pedrofurtado@ghr.com>
2025-10-18 14:43:44 -0300

    chore: Adiciona log de debug final para SEntry DSN

--- src/instrument.ts
+++ src/instrument.ts
@@ -5,6 +5,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("VERIFICAÇÃO DE DEBUG: Sentry DSN lido pelo .env:", process.env.SENTRY_DSN);
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   debug: true,
